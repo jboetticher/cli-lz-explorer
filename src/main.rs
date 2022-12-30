ac449d7d256fc1fa281326ea6b87be97fa571ea1
Jeremy Boetticher <me4jeremyboett@gmail.com>
2022-12-30 13:32:04 -0500

    Added request to API

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,12 @@
 use clap::Parser;
+use reqwest::Client;
+use serde::Deserialize;
 
-fn main() {
+#[tokio::main]
+async fn main() -> Result<(), reqwest::Error> {
     let args: TransactionHashCli = TransactionHashCli::parse();
 
-    // Parse network
+    // Parse network from argument
     let network: Network = match args.network.as_str() {
         "Mainnet" | "m" | "main" | "mainnet" => Network::Mainnet,
         "Testnet" | "t" | "test" | "testnet" => Network::Testnet,
@@ -11,7 +14,23 @@ fn main() {
         _ => panic!("Incorrect network! Consider \"mainnet\", \"testnet\", or \"sandbox\"."),
     };
 
-    println!("{:?}, {:?}", args.tx_hash, network);
+    // Communicate with the API
+    let client = Client::new();
+    let url = match network {
+        Network::Mainnet => "https://api-mainnet.layerzero-scan.com",
+        Network::Testnet => "https://api-testnet.layerzero-scan.com",
+        Network::Sandbox => "https://api-sandbox.layerzero-scan.com"
+    }.to_owned() + "/tx/" + args.tx_hash.as_str();
+    let response = client.get(url).send().await?;
+
+    // println!("{:?}", &response.text().await?);
+
+    // Parse the response
+    let data = response.json::<Messages>().await?;
+
+    println!("{:?}", data);
+
+    Ok(())
 }
 
 #[derive(Debug)]
@@ -31,4 +50,24 @@ struct TransactionHashCli {
 
     /// The network type on which the transaction was on
     network: String,
+}
+
+#[derive(Debug, Deserialize)]
+struct Messages {
+    messages: Box<[Message]>
+}
+
+#[derive(Debug, Deserialize)]
+struct Message {
+    srcUaAddress: String,
+    dstUaAddress: String,
+    srcChainId: u32,
+    dstChainId: u32,
+    dstTxHash: Option<String>,
+    dstTxError: Option<String>,
+    srcTxHash: String,
+    srcBlockHash: String,
+    srcBlockNumber: String,
+    srcUaNonce: u128,
+    status: String
 }
